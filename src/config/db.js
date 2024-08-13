3bca582b7ae9a8b5d83a9cd91f66f3c1b181195b
Paulo André dos Reis Passos <108556549+pauloandrepassos@users.noreply.github.com>
2024-08-13 18:13:41 -0300

    delete

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize(process.env.PG_BANCO, process.env.PG_USUARIO, process.env.PG_SENHA, {
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
-    port: 5432,
-})
-
-module.exports = sequelize
